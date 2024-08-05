fbfb154f869bbadd8df0aa4db19730e78c78d667
Dámaris Guzmán <damarisguzman@MacBook-Air-de-Damaris.local>
2024-08-05 14:04:19 +0200

    formulario contacto funcionando

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,15 +17,37 @@ export const Contact = () => {
   const [status, setStatus] = useState({});
 
   const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
+    setFormDetails({
+      ...formDetails,
+      [category]: value
+    });
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Enviando...");
+
+    // Validación personalizada
+    if (!formDetails.firstName || !formDetails.lastName || !formDetails.email || !formDetails.phone || !formDetails.message) {
+      setStatus({ success: false, message: 'Por favor, rellena todos los campos.' });
+      setButtonText("Enviar");
+      return;
+    }
+
+    const phonePattern = /^\d+$/;
+    if (!phonePattern.test(formDetails.phone)) {
+      setStatus({ success: false, message: 'El teléfono solo debe contener números.' });
+      setButtonText("Enviar");
+      return;
+    }
+
+    const messagePattern = /^[a-zA-Z0-9\s]*$/; // Solo letras, números y espacios
+    if (!messagePattern.test(formDetails.message)) {
+      setStatus({ success: false, message: 'El mensaje solo debe contener texto y números.' });
+      setButtonText("Enviar");
+      return;
+    }
+
     let response = await fetch("http://localhost:8080/contact", {
       method: "POST",
       headers: {
@@ -37,9 +59,9 @@ export const Contact = () => {
     let result = await response.json();
     setFormDetails(formInitialDetails);
     if (result.code === 200) {
-      setStatus({ success: true, message: 'Mensaje mandado correctamente'});
+      setStatus({ success: true, message: 'Mensaje enviado correctamente' });
     } else {
-      setStatus({ success: false, message: 'Algo ha salido mal, intentalo de nuevo más tarde.'});
+      setStatus({ success: false, message: 'Algo ha salido mal, inténtalo de nuevo más tarde.' });
     }
   };
 
@@ -50,7 +72,7 @@ export const Contact = () => {
           <Col size={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
+                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us" />
               }
             </TrackVisibility>
           </Col>
@@ -58,34 +80,66 @@ export const Contact = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Contacto</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="Nombre" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lastName} placeholder="Apellido" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="phone" value={formDetails.phone} placeholder="Teléfono" onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Mensaje" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                  <h2>Contacto</h2>
+                  <form onSubmit={handleSubmit}>
+                    <Row>
+                      <Col size={12} sm={6} className="px-1">
+                        <input 
+                          type="text" 
+                          value={formDetails.firstName} 
+                          placeholder="Nombre" 
+                          onChange={(e) => onFormUpdate('firstName', e.target.value)} 
+                          required 
+                        />
                       </Col>
-                    }
-                  </Row>
-                </form>
-              </div>}
+                      <Col size={12} sm={6} className="px-1">
+                        <input 
+                          type="text" 
+                          value={formDetails.lastName} 
+                          placeholder="Apellido" 
+                          onChange={(e) => onFormUpdate('lastName', e.target.value)} 
+                          required 
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input 
+                          type="email" 
+                          value={formDetails.email} 
+                          placeholder="Email" 
+                          onChange={(e) => onFormUpdate('email', e.target.value)} 
+                          required 
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input 
+                          type="text" 
+                          value={formDetails.phone} 
+                          placeholder="Teléfono" 
+                          onChange={(e) => onFormUpdate('phone', e.target.value)} 
+                          pattern="\d+" 
+                          title="El teléfono solo debe contener números." 
+                          required 
+                        />
+                      </Col>
+                      <Col size={12} className="px-1">
+                        <textarea 
+                          rows="6" 
+                          value={formDetails.message} 
+                          placeholder="Mensaje" 
+                          onChange={(e) => onFormUpdate('message', e.target.value)} 
+                          required 
+                        ></textarea>
+                        <button type="submit"><span>{buttonText}</span></button>
+                      </Col>
+                      {
+                        status.message &&
+                        <Col>
+                          <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                        </Col>
+                      }
+                    </Row>
+                  </form>
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
